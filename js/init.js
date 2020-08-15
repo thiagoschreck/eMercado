acc7782e4f86293c2cb499c0e7f55152c676537e
Thiago Schreck Barreiro <thiago.schreck1@gmail.com>
2020-08-15 18:07:06 -0300

    fixed index.html loading before checking login

--- js/init.js
+++ js/init.js
@@ -50,14 +50,15 @@ function getFileName()
      return lastUri;
 }
 
+if (sessionStorage.getItem('usuario') === null && getFileName() != "login.html") {
+  location.replace('login.html');
+}
+if (sessionStorage.getItem('usuario') != null && getFileName() == "login.html") {
+  location.replace('index.html');
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-  if (sessionStorage.getItem('usuario') === null && getFileName() != "login.html") {
-    location.replace('login.html');
-  }
-  if (sessionStorage.getItem('usuario') != null && getFileName() == "login.html") {
-    location.replace('index.html');
-  }
 });
