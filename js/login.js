d40f632d00e1fb0c749d070763a69a2ef6494cdd
Thiago Schreck Barreiro <thiago.schreck1@gmail.com>
2020-08-16 00:38:39 -0300

    added aesthetic details

--- js/login.js
+++ js/login.js
@@ -2,6 +2,8 @@ const form = document.getElementById("form");
 const chk_remember = document.getElementById("chk_remember")
 const email = document.getElementById("email");
 const pwd = document.getElementById("pwd");
+const btn_ingresar = document.getElementById("btn_ingresar");
+const btn_registro = document.getElementById("btn_registro");
 
 form.addEventListener("submit", function (event) {
 	event.preventDefault()
@@ -18,6 +20,32 @@ form.addEventListener("submit", function (event) {
         location.href='./index.html';
     }
 });
+
+chk_remember.addEventListener("mouseover", function(event) {
+    btn_ingresar.innerHTML = "¿Guardar credenciales?";
+});
+
+chk_remember.addEventListener("mouseleave", function(event) {
+    btn_ingresar.innerHTML = "Ingresar";
+});
+
+btn_ingresar.addEventListener("mousedown", function(event){
+    btn_ingresar.setAttribute("style", "background-color: green")
+});
+
+btn_ingresar.addEventListener("mouseleave", function(event){
+    btn_ingresar.setAttribute("style", "background-color: rgb(214, 28, 108)")
+});
+
+btn_registro.addEventListener("mouseover", function(event) {
+    btn_registro.innerHTML = "Actualmente en desarrollo";
+});
+
+btn_registro.addEventListener("mouseleave", function(event) {
+    btn_registro.innerHTML = "Registrarme";
+});
+
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
