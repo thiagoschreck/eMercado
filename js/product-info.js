6657753b2a2d3f89963dd642f6d8d4dd2e764f53
Thiago Schreck Barreiro <thiago.schreck1@gmail.com>
2020-09-08 22:28:59 -0300

    fixed comments and related products

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,6 @@
 var currentProductsArray = [];
 var currentCommentsArray = [];
+var currentRelatedArray = [];
 var product = {};
 
 function showImagesGallery(array) {
@@ -25,25 +26,61 @@ function showImagesGallery(array) {
 	}
 }
 
-function showComments(array) {
+function showRelatedProducts(array){
+
+    let htmlContentToAppend = "";
+		let product1 = array[1];
+        let product2 = array[3]; 
+
+            htmlContentToAppend += `
+            <div style="display:flex;">
+            <div style="width: 250px; margin-right: 0.5rem">
+                ` + product1.name + `<br> <img class="img-fluid img-thumbnail" width="100%" src="` + product1.imgSrc + `" alt=""></img>
+            </div>
+            <div style="width: 250px">
+                ` + product2.name + `<br> <img class="img-fluid img-thumbnail" width="100%" src="` + product2.imgSrc + `" alt=""></img>
+            </div>
+            </div>
+            `
+        document.getElementById("productRelated").innerHTML = htmlContentToAppend;
+}
+
+function showComments(array){
     let htmlContentToAppend = "";
 
-    for (let i = 0; i < currentCommentsArray.length; i++) {
-        let comment = currentCommentsArray[i];
-		let user = comment.user;
-		let score = comment.score;
-        let date = comment.dateTime;
-        
-        htmlContentToAppend += 
-        `
-        <li class="list-group-item">
-            <strong>` + user + `</strong>
-             - ` + date + ` -
-             ` + score + `             
-        </li>
-        `
-    }
-    document.getElementById("comments-list").innerHTML = htmlContentToAppend;
+    for(let i = 0; i < array.length; i++){
+		let comment = array[i];  
+		let stars = "";
+
+		for (let i=0; i<comment.score; i++){
+			stars += `
+				<span class="fa fa-star checked"></span>
+			`;
+		}
+		for (let i=comment.score; i<5; i++){
+			stars += `
+				<span class="fa fa-star"></span>
+			`;
+		}
+		
+            htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                    <i class="fas fa-user"></i>`+ " " + comment.user +`
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">` + stars + `
+                            <small class="text-muted">` + comment.dateTime + ` </small>
+                        </div>
+                        <p class="mb-1">` + comment.description + `</p>
+                    </div>
+                </div>
+            </div>
+            `
+        document.getElementById("productComments").innerHTML = htmlContentToAppend;
+	}
 }
 
 
@@ -77,13 +114,28 @@ document.addEventListener("DOMContentLoaded", function (e) {
 			//Muestro las imagenes en forma de galería
 			showImagesGallery(product.images);
 		}
-    });
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-			comment = resultObj.data;
-            showComments();
+	});
+});
+	
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            currentCommentsArray = resultObj.data;
+            showComments(currentCommentsArray);
         }
     });
+});
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            currentRelatedArray = resultObj.data;
+            showRelatedProducts(currentRelatedArray);
+        }
+    });
+});
     // TODO Related products
     // 
     // getJSONData(PRODUCTS_URL).then(function(resultObj){
@@ -96,5 +148,3 @@ document.addEventListener("DOMContentLoaded", function (e) {
 	// 		);
     //     }
     // });
-
-});
