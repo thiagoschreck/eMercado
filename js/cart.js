45a8ffc532c1b2bc5a316cec5f345c0d8417f9c5
Thiago Schreck Barreiro <thiago.schreck1@gmail.com>
2020-10-11 19:10:57 -0300

    entrega 5 completa

--- js/cart.js
+++ js/cart.js
@@ -1,37 +1,108 @@
 var currentProductsArray = [];
+var currency = [];
+var price = [];
+var quantity = [];
 
 var precioCarrito = 0;
 var impuestoCarrito = 0;
 var totalCarrito = 0;
+var currentTax = 0;
 
 function loadCartList(array){
     
     let htmlContentToAppend = "";
     for (let i = 0; i < array.articles.length; i++) {
         let product = array.articles[i];
-        let id = "articleRow" + i;
-
 		htmlContentToAppend +=
             `
-            <tr id="`+ id +`">
+            <tr class="articleRow">
                 <td style="width:15%"><img width="80%" src="`+ product.src +`" alt="placeholder"></td>
                 <td>`+ product.name +`</td>
-                <td>`+ product.currency + product.unitCost +`</td>
-                <td><input type="number" id="product-count" min="1" value="`+ product.count +`"></td>
-                <td>`+ product.currency + (product.unitCost * product.count) +`</td>
+                <td class="articleCost">`+ product.currency + " " + product.unitCost +`</td>
+                <td><input class="articleQuantity" type="number" min="1" value="`+ product.count +`" style="min-width: 2em; width: 3em"></td>
+                <td class="articleTotal">`+ product.currency + " " + (product.unitCost * product.count) +`</td>
             </tr>
             `
-        ;
-
 		document.getElementById(
 			"cuerpo-carrito"
         ).innerHTML = htmlContentToAppend;
         
-        // document.getElementById(
-        //     "articleRow0"
-        // ).attributes("hidden")
+        
+        quantity[i]=product.count;
+        currency[i]=(product.currency);
+        if(currency[i]=="USD"){
+            price[i]=product.unitCost;
+            precioCarrito += price[i]*quantity[i]*42;
+        }
+        else{
+            price[i]=product.unitCost;
+            precioCarrito += price[i]*quantity[i];
+        }
+
+
+        document.getElementById("productCostText").innerHTML = "UYU " + precioCarrito;
+        updateTax(0.15)
+        updateTotalCost();
     }
+        
+    let articleRow = document.getElementsByClassName("articleRow");
+    let articleTotal = document.getElementsByClassName("articleTotal");
+        for (let i = 0; i < articleRow.length; i++){
+            articleRow[i].addEventListener("change", function(){
+                quantity[i] = document.getElementsByClassName("articleQuantity")[i].value;
+                articleTotal[i].innerHTML = currency[i] + " " + (price[i] * quantity[i]);
+                
+                precioCarrito = 0;
+                for(j = 0; j < articleRow.length; j++){
+                    if(currency[j] == "USD"){
+                        precioCarrito += Math.round(price[j]*quantity[j]*42);
+                    }
+                    else{
+                        precioCarrito += Math.round(price[j]*quantity[j]);
+                    }
+                }
+                document.getElementById("productCostText").innerHTML = "UYU " + precioCarrito;
+                switch (currentTax){
+                    case 0: 
+                        updateTax(0.15);
+                        break;
+                    case 1: 
+                        updateTax(0.07);
+                        break;
+                    case 2: 
+                        updateTax(0.05);
+                        break;
+                }
+                updateTotalCost();
+            });
+        }
 }
+
+function updateTax(percentage){
+    impuestoCarrito = Math.round(precioCarrito * percentage);
+    document.getElementById("comissionText").innerHTML = "UYU " + impuestoCarrito;
+}
+
+function updateTotalCost(){
+    totalCarrito = precioCarrito + impuestoCarrito;
+    document.getElementById("totalCostText").innerHTML = "UYU " + totalCarrito;
+}
+
+document.getElementById("premiumradio").addEventListener("change", function(){
+    updateTax(0.15);
+    updateTotalCost();
+    currentTax = 0;
+});
+document.getElementById("expressradio").addEventListener("change", function(){
+    updateTax(0.07);
+    updateTotalCost();
+    currentTax = 1;
+});
+document.getElementById("standardradio").addEventListener("change", function(){
+    updateTax(0.05);
+    updateTotalCost();
+    currentTax = 2;
+});
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -42,5 +113,9 @@ document.addEventListener("DOMContentLoaded", function(e){
 		if (resultObj.status === "ok") {
 			loadCartList(resultObj.data);
 		}
-	});
+    });
 });
+    
+// document.getElementsByTagName("input")[1].addEventListener("change", function(){
+//         alert("test");
+// });
